0743c6278059408e557d2f906508deffae865e32
Makoto <teachermako@gmail.com>
2020-02-03 16:58:27 +0200

    Updates

---
+++ precache-manifest.a2a9b44e35449e9a085e4a6b80a29753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbd915662880b65298e18530c009ae1",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "7cde58cd4529ddb23275",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5e15227f.chunk.css"
+  },
+  {
+    "revision": "5b059f59df420793ffe5",
+    "url": "/goit-react-hw-02-feedback/static/js/2.538def0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.538def0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cde58cd4529ddb23275",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fd28a996.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
